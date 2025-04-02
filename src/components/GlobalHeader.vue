2bb616cf3257ddab786e9d459adc971b6e4a8e65
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-04-02 09:48:25 -0400

    add header

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="p-4 border-b flex flex-row justify-between w-full">
+    <router-link to="/">
+      <div class="font-bold hover:bg-gray-200 p-1 text-xl rounded-md">VUE COCOMO</div>
+    </router-link>
+    <div class="content-center">
+      <LocalIcon />
+    </div>
+  </header>
+</template>
+<script setup lang="ts">
+import LocalIcon from '@/components/icon/LocalIcon.vue'
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Main from '@/views/Main.vue'
+import MainView from '@/views/MainView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,16 +7,16 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: Main,
+      component: MainView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue'),
+    // },
   ],
 })
 
