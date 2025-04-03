36e59abb4fbcc621af124e13a94535dc11ed596b
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-04-03 00:17:05 -0400

    Create form component and input component

--- src/components/modal/AModalCreate.vue
+++ src/components/modal/AModalCreate.vue
@@ -5,7 +5,27 @@
     </template>
 
     <template v-slot:default>
-
+      <form class="w-2/3 space-y-6" @submit="onSubmit">
+        <FormField v-slot="{ componentField }" name="username" :validate-on-blur="!isFieldDirty">
+          <FormItem>
+            <FormLabel>Nombre</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+          <FormItem>
+            <FormLabel>Nombre</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+        <Button type="submit">
+          Submit
+        </Button>
+      </form>
     </template>
   </GlobalDrawer>
 </template>
@@ -13,9 +33,43 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
 import GlobalDrawer from '@/components/GlobalDrawer.vue'
+import { Input } from '@/components/ui/input'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+
 
 defineProps<{
   title: string
   description?: string
 }>()
+
+
+
+import { toTypedSchema } from '@vee-validate/zod'
+import { useForm } from 'vee-validate'
+import { h } from 'vue'
+import * as z from 'zod'
+
+const formSchema = toTypedSchema(z.object({
+  username: z.string().min(2).max(50),
+}))
+
+const { isFieldDirty, handleSubmit } = useForm({
+  validationSchema: formSchema,
+})
+
+const onSubmit = handleSubmit((values) => {
+  alert("asdasdas")
+  // toast({
+  //   title: 'You submitted the following values:',
+  //   description: h('pre', { class: 'mt-2 w-[340px] rounded-md bg-slate-950 p-4' }, h('code', { class: 'text-white' }, JSON.stringify(values, null, 2))),
+  // })
+})
 </script>
