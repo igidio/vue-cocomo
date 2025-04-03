9dddac440598e1e282576cd1896eee8ff7a9f38d
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-04-02 23:35:55 -0400

    Create accordion component and place description from A step

---
+++ src/components/GlobalAccordion.vue
@@ -0,0 +1,22 @@
+<template>
+  <Accordion
+    type="single"
+    collapsible
+    class="bg-gray-50 p-2 rounded-md border border-gray-300"
+  >
+    <AccordionItem value="item-1">
+      <AccordionTrigger>{{ trigger }}</AccordionTrigger>
+      <AccordionContent>
+        <slot/>
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+</template>
+
+<script setup lang="ts">
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+
+defineProps<{
+  trigger: string
+}>()
+</script>
--- src/components/steps/AStep.vue
+++ src/components/steps/AStep.vue
@@ -1,20 +1,49 @@
 <template>
   <span class="font-bold text-3xl mb-3">Cálculo de PFSA</span>
-  <div>
-  <p>Los PFSA se calculan identificando las funciones del sistema, como entradas, salidas, consultas, archivos internos y archivos externos.</p>
-  <p>Asigna un pesa a cada función según su complejidad (baja, media, alta) y suma los valores para obtener el PFSA, donde:</p>
-  <ul>
-    <li><b>Entradas externas (EI):</b> Datos que ingresan al sistema desde el usuario o sistemas externos.</li>
-    <li><b>Salidas externas (EO):</b> Información que el sistema genera y envía al usuario o sistemas externos.</li>
-    <li><b>Consultas externas (EQ):</b> Interacciones que permiten al usuario obtener información específica.</li>
-    <li><b>Ficheros lógicos internos (ILF):</b> Bases de datos o archivos gestionados por el sistema.</li>
-    <li><b>Ficheros lógicos externos (ELF):</b> Bases de datos o archivos gestionados por sistemas externos pero utilizados por el sistema.</li>
-  </ul>
-    <p>Es importante asignar complejidad, según los criterios como el número de campos o el nivel de interacción. De acuerdo ala complejidad es que se asigna un peso para cada función del sistema.</p>
-  </div>
+  <GlobalAccordion trigger="Descripción">
+    <div>
+      <p>
+        Los PFSA se calculan identificando las funciones del sistema, como entradas, salidas,
+        consultas, archivos internos y archivos externos.
+      </p>
+      <p>
+        Asigna un pesa a cada función según su complejidad (baja, media, alta) y suma los valores
+        para obtener el PFSA, donde:
+      </p>
+      <ul>
+        <li>
+          <b>Entradas externas (EI):</b> Datos que ingresan al sistema desde el usuario o sistemas
+          externos.
+        </li>
+        <li>
+          <b>Salidas externas (EO):</b> Información que el sistema genera y envía al usuario o
+          sistemas externos.
+        </li>
+        <li>
+          <b>Consultas externas (EQ):</b> Interacciones que permiten al usuario obtener información
+          específica.
+        </li>
+        <li>
+          <b>Ficheros lógicos internos (ILF):</b> Bases de datos o archivos gestionados por el
+          sistema.
+        </li>
+        <li>
+          <b>Ficheros lógicos externos (ELF):</b> Bases de datos o archivos gestionados por sistemas
+          externos pero utilizados por el sistema.
+        </li>
+      </ul>
+      <p>
+        Es importante asignar complejidad, según los criterios como el número de campos o el nivel
+        de interacción. De acuerdo ala complejidad es que se asigna un peso para cada función del
+        sistema.
+      </p>
+    </div>
+  </GlobalAccordion>
 
   <GlobalTable :headers="columns" :data="mapData"></GlobalTable>
-  <div class="text-end flex flex-col bg-gray-100 p-2 rounded-md w-1/2 self-end border border-gray-300 gap-1">
+  <div
+    class="text-end flex flex-col bg-gray-100 p-2 rounded-md w-1/2 self-end border border-gray-300 gap-1"
+  >
     <div v-for="(res, index) in typeResults" :key="index">
       {{ res.label }} <span class="font-bold">{{ res.value }}</span>
     </div>
@@ -32,6 +61,7 @@ import { getComplexity } from '@/data/objects/get_complexity.ts'
 import type { header_column_interface, table_data_interface } from '@/data/interfaces'
 import { useProcessStore } from '@/store/process.store.ts'
 import { storeToRefs } from 'pinia'
+import GlobalAccordion from '@/components/GlobalAccordion.vue'
 
 const { a_step, ufp_result, typeResults } = storeToRefs(useProcessStore())
 
@@ -59,7 +89,7 @@ const columns: header_column_interface[] = [
   },
 ]
 
-const mapData:ComputedRef<table_data_interface[][]> = computed(() => {
+const mapData: ComputedRef<table_data_interface[][]> = computed(() => {
   return a_step.value.map((item, index) => [
     {
       class: 'font-medium',
@@ -76,20 +106,24 @@ const mapData:ComputedRef<table_data_interface[][]> = computed(() => {
     {
       class: 'text-center',
       value: item.weight === WeightEnum.LOW ? '✅' : '',
-      tooltip: (item.weight === WeightEnum.LOW) && `Peso ${item.weight.toLowerCase()}: ${getComplexity[item.type][item.weight]}`
+      tooltip:
+        item.weight === WeightEnum.LOW &&
+        `Peso ${item.weight.toLowerCase()}: ${getComplexity[item.type][item.weight]}`,
     },
     {
       class: 'text-center',
       value: item.weight === WeightEnum.MEDIUM ? '✅' : '',
-      tooltip: (item.weight === WeightEnum.MEDIUM) && `Peso ${item.weight.toLowerCase()}: ${getComplexity[item.type][item.weight]}`
+      tooltip:
+        item.weight === WeightEnum.MEDIUM &&
+        `Peso ${item.weight.toLowerCase()}: ${getComplexity[item.type][item.weight]}`,
     },
     {
       class: 'text-center',
       value: item.weight === WeightEnum.HIGH ? '✅' : '',
-      tooltip: (item.weight === WeightEnum.HIGH) && `Peso ${item.weight.toLowerCase()}: ${getComplexity[item.type][item.weight]}`
+      tooltip:
+        item.weight === WeightEnum.HIGH &&
+        `Peso ${item.weight.toLowerCase()}: ${getComplexity[item.type][item.weight]}`,
     },
   ])
 })
-
-
 </script>
