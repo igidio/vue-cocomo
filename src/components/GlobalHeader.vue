3c1f7ba759789bf197314990b7d4775a159a03d4
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-04-14 22:24:46 -0400

    implement toggle language functionality

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -2,14 +2,14 @@
   <header
     class="p-4 border-b border-b-gray-400 flex flex-row justify-between w-full sticky top-0 bg-white z-1"
   >
-    <div class="content-center block md:hidden w-6" >
+    <div class="content-center block md:hidden w-6">
       <MenuIcon @click="is_open = true" v-if="route.name === 'create' || route.name === 'edit'" />
     </div>
     <router-link to="/">
       <div class="font-bold hover:bg-gray-200 p-1 text-xl rounded-md">VUE COCOMO</div>
     </router-link>
-    <div class="content-center" @click="$i18n.locale = 'es'">
-      <LocalIcon />
+    <div class="content-center" @click="toggle_language()">
+      <Button variant="ghost" class="font-bold"> {{ locale == 'en' ? 'ES' : 'EN' }} </Button>
     </div>
   </header>
   <GlobalSheet v-model="is_open" :title="t('side_menu.title')">
@@ -17,7 +17,7 @@
   </GlobalSheet>
 </template>
 <script setup lang="ts">
-import LocalIcon from '@/components/icon/LocalIcon.vue'
+import { Button } from '@/components/ui/button'
 import GlobalSheet from '@/components/GlobalSheet.vue'
 import SidebarContent from '@/components/steps/SidebarContent.vue'
 import MenuIcon from '@/components/icon/MenuIcon.vue'
@@ -25,8 +25,16 @@ import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 
-const {t} = useI18n()
+const { t, locale } = useI18n()
 const route = useRoute()
 
 const is_open = ref(false)
+
+const toggle_language = () => {
+  if (locale.value === 'en') {
+    locale.value = 'es'
+    return
+  }
+  locale.value = 'en'
+}
 </script>
