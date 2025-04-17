eee2e50d7f760377a693b45ae0b199124f8a2855
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-04-16 22:02:44 -0400

    Last commit

--- src/data/sample/b_step.data.ts
+++ src/data/sample/b_step.data.ts
@@ -1,58 +1,118 @@
-export default [
-  {
-    value: 'Comunicación de datos',
-    score: 3,
-  },
-  {
-    value: 'Funciones distribuidas',
-    score: 1,
-  },
-  {
-    value: 'Prestaciones de servicios',
-    score: 4,
-  },
-  {
-    value: 'Gran uso de la configuración',
-    score: 2,
-  },
-  {
-    value: 'Velocidad de transacciones',
-    score: 4,
-  },
-  {
-    value: 'Entrada de datos en línea',
-    score: 4,
-  },
-  {
-    value: 'Diseño para el usuario final',
-    score: 5,
-  },
-  {
-    value: 'Actualización de datos en línea',
-    score: 4,
-  },
-  {
-    value: 'Complejidad del proceso',
-    score: 3,
-  },
-  {
-    value: 'Reusabilidad del código',
-    score: 4,
-  },
-  {
-    value: 'Facilidad de las operaciones',
-    score: 4,
-  },
-  {
-    value: 'Facilidad de instalaciones',
-    score: 5,
-  },
-  {
-    value: 'Locaciones múltiples',
-    score: 1,
-  },
-  {
-    value: 'Facilidad de cambios',
-    score: 4,
-  },
-]
+export default {
+  es: [
+    {
+      value: 'Comunicación de datos',
+      score: 5,
+    },
+    {
+      value: 'Funciones distribuidas',
+      score: 5,
+    },
+    {
+      value: 'Prestaciones de servicios',
+      score: 5,
+    },
+    {
+      value: 'Gran uso de la configuración',
+      score: 5,
+    },
+    {
+      value: 'Velocidad de transacciones',
+      score: 5,
+    },
+    {
+      value: 'Entrada de datos en línea',
+      score: 5,
+    },
+    {
+      value: 'Diseño para el usuario final',
+      score: 5,
+    },
+    {
+      value: 'Actualización de datos en línea',
+      score: 5,
+    },
+    {
+      value: 'Complejidad del proceso',
+      score: 5,
+    },
+    {
+      value: 'Reusabilidad del código',
+      score: 5,
+    },
+    {
+      value: 'Facilidad de las operaciones',
+      score: 5,
+    },
+    {
+      value: 'Facilidad de instalaciones',
+      score: 5,
+    },
+    {
+      value: 'Locaciones múltiples',
+      score: 5,
+    },
+    {
+      value: 'Facilidad de cambios',
+      score: 5,
+    },
+  ],
+  en: [
+    {
+      value: 'Data communication',
+      score: 5,
+    },
+    {
+      value: 'Distributed functions',
+      score: 5,
+    },
+    {
+      value: 'Service features',
+      score: 5,
+    },
+    {
+      value: 'High configuration usage',
+      score: 5,
+    },
+    {
+      value: 'Transaction speed',
+      score: 5,
+    },
+    {
+      value: 'Online data entry',
+      score: 5,
+    },
+    {
+      value: 'End-user design',
+      score: 5,
+    },
+    {
+      value: 'Online data update',
+      score: 5,
+    },
+    {
+      value: 'Process complexity',
+      score: 5,
+    },
+    {
+      value: 'Code reusability',
+      score: 5,
+    },
+    {
+      value: 'Ease of operations',
+      score: 5,
+    },
+    {
+      value: 'Ease of installations',
+      score: 5,
+    },
+    {
+      value: 'Multiple locations',
+      score: 5,
+    },
+    {
+      value: 'Ease of changes',
+      score: 5,
+    },
+  ]
+}
--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -7,6 +7,7 @@ import { computed, onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useProcessStore } from '@/store/process.store.ts'
 import { useI18n } from 'vue-i18n'
+import b_stepData from '@/data/sample/b_step.data.ts'
 
 const { step_index, mode, id, a_step, b_step, name } = storeToRefs(useProcessStore())
 const { locale, t } = useI18n()
@@ -16,7 +17,7 @@ onMounted(() => {
   id.value = undefined
   step_index.value = 1
   a_step.value = []
-  b_step.value = []
+  b_step.value = locale.value === 'en' ? b_stepData.en : b_stepData.es
   name.value = locale.value === 'en' ? 'Project name' : 'Nombre del proyecto'
 })
 
