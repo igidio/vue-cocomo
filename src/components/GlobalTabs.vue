ec03ffbacc2b5340be9196a1bb8e9cb546b040a9
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-01-31 20:40:15 -0400

    fix: add overflow to globaltabs component

--- src/components/GlobalTabs.vue
+++ src/components/GlobalTabs.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tabs class="w-full flex flex-col md:flex-row" orientation="vertical" v-model="selected_model" >
+  <Tabs class="w-full flex flex-col md:flex-row" orientation="vertical" v-model="selected_model">
     <TabsList class="grid w-[220px] grid-cols-1 h-fit self-end md:self-auto">
       <TabsTrigger :value="element.value" v-for="(element, index) in list" :key="index">
         {{ element.label }}
@@ -8,10 +8,14 @@
     <TabsContent :value="value" v-for="value in list.map((item) => item.value)" :key="value">
       <Card class="p-6">
         <div>
-          <span class="font-bold border-b-black mb-2 inline-block">{{ list.find((item) => item.value === value)?.label }}</span>
+          <span class="font-bold border-b-black mb-2 inline-block">{{
+            list.find((item) => item.value === value)?.label
+          }}</span>
           <hr />
         </div>
-        <slot :name="value" />
+        <div class="w-full overflow-x-auto">
+          <slot :name="value" />
+        </div>
       </Card>
     </TabsContent>
   </Tabs>
@@ -22,8 +26,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Card } from '@/components/ui/card'
 import { ref } from 'vue'
 
-
-const selected_model = ref("ufp")
+const selected_model = ref('ufp')
 
 defineProps<{
   list: {
--- src/components/read/UfpSection.vue
+++ src/components/read/UfpSection.vue
@@ -1,5 +1,5 @@
 <template>
-    <table class="min-w-full divide-y divide-gray-200">
+    <table class="w-full divide-y divide-gray-200">
       <thead>
       <tr>
         <th class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase">
